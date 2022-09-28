435d69768288885281f69d43be66bc7fb8152bee
mohamednagy54 <92611426+mohamednagy54@users.noreply.github.com>
2022-09-28 11:08:31 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,24 @@
+
+
+
+const linksMenu = document.querySelector('.links')
+const ulElement = document.getElementById('list')
+const spanEl = document.querySelector('.two')
+linksMenu.addEventListener('click',(e) => {
+    
+    spanEl.classList.toggle('active')
+    ulElement.classList.toggle('active')
+    
+})
+
+
+
+
+
+
+
+
+
+
+
+
